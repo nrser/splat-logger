95740d78b4ffdc3fa6aede94dd47841b584da75e
nrser <neil@neilsouza.com>
2022-09-01 01:22:41 -0700

    More json work

--- splatlog/json/json_encoder.py
+++ splatlog/json/json_encoder.py
@@ -5,6 +5,8 @@ from typing import Type
 from collections.abc import Collection, Mapping
 from enum import Enum
 
+from splatlog.lib import required_arity
+
 
 def encode_class(cls: Type) -> str:
     if cls.__module__ == "builtins":
@@ -14,7 +16,37 @@ def encode_class(cls: Type) -> str:
 
 class JSONEncoder(json.JSONEncoder):
     """
+    An extension of `json.JSONEncoder` that attempts to deal with all the crap
+    you might splat into a log.
+
     ##### Examples #####
+
+    Presented in resolution order — first one that applies wins... or loses; if
+    that path fails for some reason, we don't keep trying down-list.
+
+    ###### Specific Handler #######
+
+    Any object can implement a `to_json_default` method, and that will be used.
+
+    ```python
+
+    >>> class A:
+    ...     def __init__(self, x, y, z):
+    ...         self.x = x
+    ...         self.y = y
+    ...         self.z = z
+    ...
+    ...     def to_json_default(self):
+    ...         return dict(x=self.x, y=self.y, z=self.z)
+
+    >>> json.dump(A(x=1, y=2, z=3), stdout, cls=JSONEncoder, indent=4)
+    {
+        "x": 1,
+        "y": 2,
+        "z": 3
+    }
+
+    ```
 
     ###### Classes ######
 
@@ -28,11 +60,11 @@ class JSONEncoder(json.JSONEncoder):
 
     >>> from sys import stdout
 
-    >>> class A:
+    >>> class B:
     ...     pass
 
-    >>> json.dump(A, stdout, cls=JSONEncoder, indent=4)
-    "splatlog.json.json_encoder.A"
+    >>> json.dump(B, stdout, cls=JSONEncoder, indent=4)
+    "splatlog.json.json_encoder.B"
 
     ```
 
@@ -46,6 +78,110 @@ class JSONEncoder(json.JSONEncoder):
 
     >>> json.dump(str, stdout, cls=JSONEncoder, indent=4)
     "str"
+
+    ```
+
+    ###### Dataclasses ######
+
+    Dataclass instances are encoded via `dataclasses.asdict`.
+
+    ```python
+
+    >>> @dataclasses.dataclass
+    ... class DC:
+    ...     x: int
+    ...     y: int
+    ...     z: int
+
+    >>> json.dump(DC(x=1, y=2, z=3), stdout, cls=JSONEncoder, indent=4)
+    {
+        "x": 1,
+        "y": 2,
+        "z": 3
+    }
+
+    ```
+
+    ###### Enums ######
+
+    Instances of `enum.Enum` are encoded _nominally_ as JSON strings, composed
+    of the class of the object (per class encoding, discussed above) and the
+    object's `name`, joined (again) with a `.`.
+
+    ```python
+
+    >>> from enum import Enum
+
+    >>> class Status(Enum):
+    ...     OK = "ok"
+    ...     ERROR = "error"
+
+    >>> json.dump(Status.OK, stdout, cls=JSONEncoder, indent=4)
+    "splatlog.json.json_encoder.Status.OK"
+
+    ```
+
+    Note that enums that descend from `enum.IntEnum` are automatically encoded
+    by _value_ via the standard JSON encoder.
+
+    ```python
+
+    >>> from enum import IntEnum
+
+    >>> class IntStatus(IntEnum):
+    ...     OK = 200
+    ...     ERROR = 500
+
+    >>> json.dump(IntStatus.OK, stdout, cls=JSONEncoder, indent=4)
+    200
+
+    ```
+
+    ###### General Conversion Methods #######
+
+    The encoder looks for zero-arity methods named `to_dict`, `to_tuple` and
+    `to_list` (in that order), and calls the first one it finds, expecting it
+    to return the obvious result.
+
+    This basically allows you to have your classes do dataclass-like encoding.
+
+    ```python
+
+    >>> class ToDict:
+    ...     def __init__(self, x, y, z):
+    ...         self.x = x
+    ...         self.y = y
+    ...         self.z = z
+    ...
+    ...     def to_dict(self):
+    ...         return self.__dict__
+
+    >>> json.dump(ToDict(1, 2, 3), stdout, cls=JSONEncoder)
+    {"x": 1, "y": 2, "z": 3}
+
+    >>> class ToTuple:
+    ...     def __init__(self, x, y, z):
+    ...         self.x = x
+    ...         self.y = y
+    ...         self.z = z
+    ...
+    ...     def to_tuple(self):
+    ...         return (self.x, self.y, self.z)
+
+    >>> json.dump(ToTuple(1, 2, 3), stdout, cls=JSONEncoder)
+    [1, 2, 3]
+
+    >>> class ToList:
+    ...     def __init__(self, x, y, z):
+    ...         self.x = x
+    ...         self.y = y
+    ...         self.z = z
+    ...
+    ...     def to_list(self):
+    ...         return [self.x, self.y, self.z]
+
+    >>> json.dump(ToList(1, 2, 3), stdout, cls=JSONEncoder)
+    [1, 2, 3]
 
     ```
 
@@ -90,45 +226,20 @@ class JSONEncoder(json.JSONEncoder):
     }
 
     ```
-
-    ###### Enums ######
-
-    Instances of `enum.Enum` are encoded _nominally_ as JSON strings, composed
-    of the class of the object (per class encoding, discussed above) and the
-    object's `name`, joined (again) with a `.`.
-
-    ```python
-
-    >>> from enum import Enum
-
-    >>> class Status(Enum):
-    ...     OK = "ok"
-    ...     ERROR = "error"
-
-    >>> json.dump(Status.OK, stdout, cls=JSONEncoder, indent=4)
-    "splatlog.json.json_encoder.Status.OK"
-
-    ```
-
-    Note that enums that descend from `enum.IntEnum` are automatically encoded
-    by _value_ via the standard JSON encoder.
-
-    ```python
-
-    >>> from enum import IntEnum
-
-    >>> class IntStatus(IntEnum):
-    ...     OK = 200
-    ...     ERROR = 500
-
-    >>> json.dump(IntStatus.OK, stdout, cls=JSONEncoder, indent=4)
-    200
-
-    ```
-
     """
 
+    CONVERSION_METHOD_NAMES = [
+        "to_dict",
+        "to_tuple",
+        "to_list",
+    ]
+
     def default(self, obj):
+        if hasattr(obj, "to_json_default"):
+            attr = getattr(obj, "to_json_default")
+            if ismethod(attr) and required_arity(attr) == 0:
+                return attr()
+
         if isclass(obj):
             return encode_class(obj)
 
@@ -138,20 +249,11 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(obj, Enum):
             return f"{encode_class(obj.__class__)}.{obj.name}"
 
-        if hasattr(obj, "to_dict"):
-            to_dict = getattr(obj, "to_dict")
-            if ismethod(to_dict):
-                return to_dict()
-
-        if hasattr(obj, "to_tuple"):
-            to_tuple = getattr(obj, "to_tuple")
-            if ismethod(to_tuple):
-                return to_tuple()
-
-        if hasattr(obj, "to_list"):
-            to_list = getattr(obj, "to_list")
-            if ismethod(to_list):
-                return to_list()
+        for method_name in self.__class__.CONVERSION_METHOD_NAMES:
+            if hasattr(obj, method_name):
+                attr = getattr(obj, method_name)
+                if ismethod(attr) and required_arity(attr) == 0:
+                    return attr()
 
         if isinstance(obj, Mapping):
             return {
