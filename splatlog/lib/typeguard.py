7bd81893e17bfeff1110dbe0cf09db0dc7d9f83f
nrser <neil@neilsouza.com>
2023-01-16 15:08:35 -0800

    Upgrade to typeguard 3.0.0b2 to support types.UnionType

--- splatlog/lib/typeguard.py
+++ splatlog/lib/typeguard.py
@@ -1,12 +1,12 @@
 from typing import Any, TypeGuard, TypeVar
-from typeguard import check_type
+from typeguard import check_type, TypeCheckError
 
 T = TypeVar("T")
 
 
 def satisfies(value: Any, expected_type: type[T]) -> TypeGuard[T]:
     try:
-        check_type("value", value, expected_type)
-    except TypeError:
+        check_type(value, expected_type)
+    except TypeCheckError:
         return False
     return True
